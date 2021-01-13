c3c22fd3fb508f0a0e4cbdab584ed2dc251bebba
Kshitij Tiwari <kshitij@localhost.localdomain>
2021-01-13 10:08:00 +0530

    Made a program to find interest.

---
+++ Interest.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int p;
+    int r;
+    int t;
+    int i;
+    int a;
+    cin >> p;
+    cin >> r;
+    cin >> t;
+    i=p*r/100*t;
+    a=p+i;
+    cout << a;
+}
